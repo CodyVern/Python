1ba729e0675f04bc392e9eff7ba709bdd5ef3035
Cody Verner <cvern0@outlook.com>
2022-10-03 14:32:28 +0930

    Sort Algorithim - Full re-iteration

--- Sorting_Algorithim.py
+++ Sorting_Algorithim.py
@@ -2,15 +2,19 @@ test = [12, 4, 3, 2, 8]
 
 def Sorter(Nums):
     lengh = len(Nums)-1
-
-    for i in range(len(Nums)):
-        temp = 0
-        if i < lengh:
-            if Nums[i] > Nums[i+1]:
-                temp = Nums[i+1]
-                Nums[i+1] = Nums[i]
-                Nums[i] = temp
-                print(Nums[i], "Swapped with", Nums[i+1])
+    x = 1
+    while x > 0:
+        x = 0
+        for i in range(len(Nums)):
+            temp = 0
+            if i < lengh:
+                if Nums[i] > Nums[i+1]:
+                    x = 1
+                    temp = Nums[i+1]
+                    Nums[i+1] = Nums[i]
+                    Nums[i] = temp
+                    print(Nums[i], "Swapped with", Nums[i+1])
+            
 
 Sorter(test)
 
