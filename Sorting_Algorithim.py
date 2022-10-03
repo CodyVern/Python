f48ce9198bc369134f944822955ac572c1e9f480
Cody Verner <cvern0@outlook.com>
2022-10-03 15:11:18 +0930

    Comments added

--- Sorting_Algorithim.py
+++ Sorting_Algorithim.py
@@ -2,19 +2,18 @@ test = [12, 4, 3, 2, 8]
 
 def Sorter(Nums):
     lengh = len(Nums)-1
-    x = 1
+    x = 1       #Set to initialize while loop
     while x > 0:
-        x = 0
+        x = 0   #Loop will now stop unless defined otherwise
         for i in range(len(Nums)):
-            temp = 0
-            if i < lengh:
-                if Nums[i] > Nums[i+1]:
-                    x = 1
+            temp = 0 #empty var to swap integers
+            if i < lengh: #if i is > length during below's if it'll error out
+                if Nums[i] > Nums[i+1]: #Number unsorted in list 
+                    x = 1 #Loop will now re-iterate through list
                     temp = Nums[i+1]
                     Nums[i+1] = Nums[i]
                     Nums[i] = temp
-                    print(Nums[i], "Swapped with", Nums[i+1])
-            
+                    print(Nums[i+1], "Swapped with", Nums[i])
 
 Sorter(test)
 
